50277adf988418cb46ce608302347492094e2b90
tybruno <tybruno@cisco.com>
2021-08-06 11:24:30 -0700

    refactoring

--- file_reader/file_readers.py
+++ file_reader/file_readers.py
@@ -1,7 +1,14 @@
+from abc import (
+    ABC,
+    abstractmethod,
+)
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Union, Iterable, Callable
-from dataclasses import dataclass, field
-from abc import ABC, abstractmethod
+from typing import (
+    Callable,
+    Iterable,
+    Union,
+)
 
 
 class FileReaderError(Exception):
